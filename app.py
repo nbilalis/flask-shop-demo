a99bb497aa8059dc55d257353e08399fa0e776e0
Nicholaos Bilalis <n.bilalis@sae.edu>
2022-06-04 12:36:04 +0300

    Add "product details" view function

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, g
 from os import environ
 import locale
 
-from products_service import get_all_products, get_category, get_all_categories
+from products_service import get_product, get_all_products, get_category, get_all_categories
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    categories = get_all_categories()
+
     return render_template(
         'home.html',
+        men_category=get_category('men', categories),
+        women_category=get_category('women', categories),
+        sports_category=get_category('sports', categories),
         men_products=get_all_products(8),
         women_products=get_all_products(8),
         sports_products=get_all_products(8)
@@ -21,9 +26,27 @@ def home():
 def product_list(category_id):
     category = get_category(category_id, get_all_categories())
     products = get_all_products()
+
     app.logger.debug(category)
     app.logger.debug(products)
+
     return render_template('products/list.html', category=category, products=products)
+
+
+@app.route('/products/<category_id>/<product_id>')
+def product_details(category_id, product_id):
+    category = get_category(category_id, get_all_categories())
+    product = get_product(product_id)
+
+    app.logger.debug(category)
+    app.logger.debug(product)
+
+    return render_template('products/details.html', category=category, product=product)
+
+
+@app.route('/products/<product_id>/api')
+def get_product_json(product_id):
+    return get_product(product_id)
 
 
 @app.errorhandler(404)
